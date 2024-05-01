d098f594c78252c6290b8bb6793a78d36168306b
Gmarques023 <153223582+Gmarques023@users.noreply.github.com>
2024-05-01 18:13:41 +0000

    update diagrams in models.py

--- src/models.py
+++ src/models.py
@@ -1,29 +1,72 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy.sql import func
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    profile_picture = Column(String(255))
+    bio = Column(String(255))
+    followers_count = Column(Integer, default=0)
+    following_count = Column(Integer, default=0)
+    posts = relationship("Post", back_populates="user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table post.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    User_id = Column(Integer, ForeignKey('user.id'))
+    image_url = Column(String(255),nullable=False)
+    caption = Column(String(255))
+    likes_count = Column(Integer, default=0)
+    creation_date = Column(DateTime(timezone=True), server_default=func.now())
+    user = relationship("User",back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
+
+class Comment(Base):
+    # Here we define columns for the table Comment.
+    # Notice that each column is also a normal Python instance attribute.
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    text = Column(String(255))
+    creation_date = Column(DateTime(timezone=True), server_default=func.now())
+    user = relationship("User")
+    post = relationship("Post", back_populates=("comments"))
+
+class Like(Base):
+    __tablename__='like'
+    # Here we define columns for the table like.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    creation_date = Column(DateTime(timezone=True), server_default=func.now())
+
+class Share(Base):
+    __tablename__ = 'share'
+    # Here we define columns for the table share.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    creation_date = Column(DateTime(timezone=True), server_default=func.now())
+    user = relationship("User")
+    post = relationship("Post")
+
 
     def to_dict(self):
         return {}
